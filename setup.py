c5753af4ebbd6744a90294baed3ff63e2a89f257
Jonathan <KuugJonathan45@gmail.com>
2022-11-10 20:53:05 +0000

    alpha commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Training a machine learning model to predict sentiment polarity in product reviews',
+    author='Jonathan Kuug',
+    license='',
+)
